90d8b4513ddeddaf0e2fd144af8e71272f36d10e
gabrielsabaini <gabriel.tesch@gmail.com>
2023-10-25 16:18:05 -0300

    Initial Commit

---
+++ Componentizacao/ComponentizacaoApp.swift
@@ -0,0 +1,17 @@
+//
+//  ComponentizacaoApp.swift
+//  Componentizacao
+//
+//  Created by Gabriel Sabaini on 25/10/23.
+//
+
+import SwiftUI
+
+@main
+struct ComponentizacaoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
